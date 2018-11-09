50a981d2228b467898f4f5e93cdf007136faf6c2
Pawel Rudnicki <p.j.rudnicki@gmail.com>
2018-11-09 13:46:45 +0000

    Added initial code base

---
+++ VisualTreeTraversionPoC/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Shapes;
+using VisualTreeTraversionPoC.Utils;
+
+namespace VisualTreeTraversionPoC
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Rectangle_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (sender is Rectangle rectangle)
+            {
+                var stackPanel = VisualTreeTraverseHelper.FindParent<StackPanel>(rectangle);
+                if (stackPanel != null)
+                {
+                    // we've got this!
+                }
+
+                var grid = VisualTreeTraverseHelper.FindParent<Grid>(rectangle);
+                if (grid != null)
+                {
+                    // we've got this!
+                }
+            }
+        }
+    }
+}
---
+++ VisualTreeTraversionPoC/Utils/VisualTreeTraverseHelper.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+using System.Windows.Media;
+
+namespace VisualTreeTraversionPoC.Utils
+{
+    public static class VisualTreeTraverseHelper
+    {
+        public static T FindParent<T>(this DependencyObject child)
+            where T : DependencyObject
+        {
+            var parentObject = VisualTreeHelper.GetParent(child);
+            if (parentObject == null) return null;
+
+            if (parentObject is T parent)
+            {
+                return parent;
+            }
+            else
+            {
+                return FindParent<T>(parentObject);
+            }
+        }
+    }
+}
